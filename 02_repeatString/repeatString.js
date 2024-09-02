f4c006418505870bbc559cdf38a7f9d86388368b
Fatimaaasaleem <162432842+Fatimaaasaleem@users.noreply.github.com>
2024-09-02 19:27:35 +0500

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
 const repeatString = function() {
+let string="hey";
+  console.log(string)
 
 };
+for(let i=0;i<=3:i++){
+repeatString();
+}
 
 // Do not edit below this line
 module.exports = repeatString;
